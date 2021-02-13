2b2fd37f90cbb0344c21d17703a788501f28e612
kevin.suedmersen <kevin.suedmersen@breos.com>
2021-02-13 12:03:45 +0100

    added profiling code

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,13 @@
 import pandas as pd
+from pandas_profiling import ProfileReport
+
 
 class Dataset:
     def __init__(self, filepath):
         self.filepath = filepath
         self.df = pd.read_csv(filepath)
         print(F'Read dataframe from {filepath} into memory')
+
+    def profile(self, title='Dataset profile report'):
+        profile = ProfileReport(self.df, title=title)
+        profile.to_notebook_iframe()
