935af1c4c533be6a2ad7c6f3d34be94aafd27cc9
kevin.suedmersen <kevin.suedmersen@breos.com>
2021-02-16 23:39:13 +0100

    added docstrings to preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,6 +10,17 @@ pd.options.display.width = 0
 
 class Dataset:
     def __init__(self, df_path_train: str, df_path_test: str, id_col: str, ground_truth: str):
+        """Instantiates a dataset and concatenates the training and test set for joint preprocessing
+
+        :param df_path_train: Path to training data
+        :type df_path_train: str
+        :param df_path_test: Path to test data
+        :type df_path_test: str
+        :param id_col: Id column
+        :type id_col: str
+        :param ground_truth: Ground truth column
+        :type ground_truth: str
+        """
         self.df_path_train = df_path_train
         self.df_path_test = df_path_test
         self.id_col = id_col
@@ -19,6 +30,8 @@ class Dataset:
         self._union_data()
 
     def _union_data(self):
+        """Vertically concatenates the training and test data for joint preprocessing
+        """
         df_train = pd.read_csv(self.df_path_train)
         logger.info(f'Train samples: "{len(df_train)}"')
         df_test = pd.read_csv(self.df_path_test)
@@ -52,14 +65,14 @@ class Dataset:
             logger.info(f'A profiling report was already generated and will be loaded from ``{html_path}``')
             display(IFrame(src=html_path, width=10**3, height=10**3))
 
-    def _fill_missing_values(self, col_name_to_fill_method: dict):
-        """Fills missing values
+    def _fill_missing_values(self, missing_value_config: dict):
+        """Fills missing values as specified in missing_value_config
 
-        :param col_name_to_fill_method: Dictionary mapping column name to method by which missing values
-            are filled
-        :return: None
+        :param missing_value_config: Dictionary mapping column name to missing value filling method
+        :type missing_value_config: dict
+        :raises ValueError: Raises ValueError when unknown filling method is provided
         """
-        for col_name, fill_method in col_name_to_fill_method.items():
+        for col_name, fill_method in missing_value_config.items():
 
             # Calculate the fill value
             if fill_method == 'median':
@@ -77,13 +90,14 @@ class Dataset:
             logger.info(f'The {fill_method} of column ``{col_name}`` equals: {fill_value}')
             self.data[col_name].fillna(fill_value, inplace=True)
 
-    def _encode_categories(self, col_name_to_encoding):
-        """Encodes categorical variables
+    def _encode_categories(self, encoding_config: dict):
+        """Encodes categorical variables according to encoding_config
 
-        :param col_name_to_encoding: Dictionary describing which variable should be encoded in what way
-        :return: None
+        :param encoding_config: Dict mapping column name to encoding method
+        :type encoding_config: dict
+        :raises ValueError: If unknown encoding method is provided
         """
-        for col_name, encoding in col_name_to_encoding.items():
+        for col_name, encoding in encoding_config.items():
             if isinstance(encoding, dict):
                 self.data[col_name] = self.data[col_name].map(encoding)
                 logger.info(f'Converted column ``{col_name}`` using the custom mapping ``{encoding}``')
@@ -94,17 +108,18 @@ class Dataset:
             else:
                 raise ValueError('Unknown category encoding provided: ``{encoding}``')
 
-    def do_basic_preprocessing(self, col_name_to_fill_method: dict, col_name_to_encoding: dict):
-        """Conducts basic preprocessing, i.e. replaces missing values and encodes categorical values
+    def do_basic_preprocessing(self, missing_value_config: dict, encoding_config: dict):
+        """Conducts basic preprocessing, i.e. fills missing values and encodes existing, categorical 
+        variables
 
-        :param col_name_to_fill_method: Dictionary describing which column's missing values should
-            be filled and how
-        :param col_name_to_encoding: Dictionary describing which column's categorical values should
-            be encoded and how
+        :param missing_value_config: Dict mapping column name to filling method
+        :type missing_value_config: dict
+        :param encoding_config: Dict mapping colname to encoding method
+        :type encoding_config: dict
         """
         if not self.basic_preprocessing_finished:
-            self._fill_missing_values(col_name_to_fill_method)
-            self._encode_categories(col_name_to_encoding)
+            self._fill_missing_values(missing_value_config)
+            self._encode_categories(encoding_config)
             self.basic_preprocessing_finished = True
             logger.info('Preprocessing finished')
             logger.info(f'Available columns: {list(self.data.columns)}')
@@ -112,10 +127,15 @@ class Dataset:
             logger.info('Preprocessing was already conducted')
     
     def drop_cols(self, cols_to_drop: list, mode: str='training'):
-        """Returns a subset of the dataframe
+        """Drops a subset of cols
 
-        :param predictors: List of predictors to include
-        :return: Dataframe subset
+        :param cols_to_drop: Cols to drop
+        :type cols_to_drop: list
+        :param mode: Whether we are training or testing/predicting, defaults to 'training'
+        :type mode: str, optional
+        :raises ValueError: If unknown mode is provided
+        :return: The dataframe subset
+        :rtype: pd.DataFrame
         """
         cols_to_keep = [col for col in self.data.columns if col not in cols_to_drop]
         if mode == 'training':
@@ -125,25 +145,55 @@ class Dataset:
             rows_to_keep = self.data[self.ground_truth].isna()
             df_subset = self.data.loc[rows_to_keep, cols_to_keep]
         else:
-            raise ValueError(f'Unknown model provided: "{mode}"')
+            raise ValueError(f'Unknown mode provided: "{mode}"')
         return df_subset
 
-    def _gen_continuous_vars(self, name_col, sibbling_spouse_col, parent_child_col):
+    def _gen_continuous_vars(self, name_col: str, sibbling_spouse_col: str, parent_child_col: str):
+        """Generates continuous variables
+
+        :param name_col: Name of name col
+        :type name_col: str
+        :param sibbling_spouse_col: Name of SibSp col
+        :type sibbling_spouse_col: str
+        :param parent_child_col: Name of Parch col
+        :type parent_child_col: str
+        """
         # Generate continuous variables
         self.data['name_len'] = self.data[name_col].apply(lambda name: len(name.split()))
         self.data['family_size'] = self.data[sibbling_spouse_col] + self.data[parent_child_col]
     
-    def _gen_indicator_vars(self, cabin_col):
+    def _gen_indicator_vars(self, cabin_col: str):
+        """Generates indicator variables
+
+        :param cabin_col: Cabin col
+        :type cabin_col: str
+        """
         # Generate indicators
         self.data['has_cabin'] = self.data[cabin_col].apply(lambda cabin: 1 if cabin is not None else 0)
         self.data['is_alone'] = self.data['family_size'].apply(lambda fam_size: 1 if fam_size == 0 else 0)
 
-    def _bin_continuous_vars(self, fare_col, age_col, n_bins: int=4):
+    def _bin_continuous_vars(self, fare_col: str, age_col: str, n_bins: int=4):
+        """Bins continuous vars
+
+        :param fare_col: Far col
+        :type fare_col: str
+        :param age_col: Age col
+        :type age_col: str
+        :param n_bins: Number of bins, defaults to 4
+        :type n_bins: int, optional
+        """
         # Bin continuous variables
         self.data['fare_category'] = pd.cut(self.data[fare_col], bins=n_bins, labels=False)
         self.data['age_category'] = pd.cut(self.data[age_col], bins=n_bins, labels=False)
 
-    def _extract_title_from_name(self, name_col):
+    def _extract_title_from_name(self, name_col: str):
+        """Extracts the title from the name column. Also based on:
+
+            - https://www.kaggle.com/imoore/titanic-the-only-notebook-you-need-to-see
+
+        :param name_col: Name col
+        :type name_col: str
+        """
         # Extract the title from the name column
         self.data['title'] = self.data[name_col].str.extract(' ([A-Za-z]+)\.', expand=False) # (\w+\.) matches the first word ending with a dot character
         self.data['title'] = self.data['title'].str.lower()
@@ -156,11 +206,13 @@ class Dataset:
         dummy_titles = pd.get_dummies(self.data['title'], prefix='title')
         self.data = pd.concat([self.data, dummy_titles], axis=1)
 
-    def _extract_ticket_info(self, ticket_col):
+    def _extract_ticket_info(self, ticket_col: str):
         """Extracts information from the ticket column. Mostly based on: 
+            
+            - https://www.kaggle.com/pliptor/titanic-ticket-only-study
 
-        - https://www.kaggle.com/pliptor/titanic-ticket-only-study
-        :return: None
+        :param ticket_col: Ticket col
+        :type ticket_col: str
         """
         # For each ticket, count its number of duplicates
         n_duplicate_tickets = self.data.groupby(ticket_col).size()
@@ -205,10 +257,22 @@ class Dataset:
         age_col: str='Age',
         ticket_col: str='Ticket'
     ):
-        """More advanced preprocessing based on:
+        """Conducts advanced preprocessing and feature engineering
 
-        - https://www.kaggle.com/imoore/titanic-the-only-notebook-you-need-to-see
-        - https://www.kaggle.com/startupsci/titanic-data-science-solutions
+        :param name_col: Name col, defaults to 'Name'
+        :type name_col: str, optional
+        :param cabin_col: Cabin col, defaults to 'Cabin'
+        :type cabin_col: str, optional
+        :param sibbling_spouse_col: SibSp col, defaults to 'SibSp'
+        :type sibbling_spouse_col: str, optional
+        :param parent_child_col: Parch col, defaults to 'Parch'
+        :type parent_child_col: str, optional
+        :param fare_col: Fare col, defaults to 'Fare'
+        :type fare_col: str, optional
+        :param age_col: Age col, defaults to 'Age'
+        :type age_col: str, optional
+        :param ticket_col: Ticket col, defaults to 'Ticket'
+        :type ticket_col: str, optional
         """
         if not self.advanced_preprocessing_finished:
             self._gen_continuous_vars(name_col, sibbling_spouse_col, parent_child_col)
